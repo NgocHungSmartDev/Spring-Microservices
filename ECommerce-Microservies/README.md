c869b382dcbb00fc21a4127402482ff4b784094d
NgocHungSmartDev <hoang.ngoc.hungit@gmail.com>
2023-02-26 16:21:05 +0700

    Microservices | apply Security Eureka server

--- ECommerce-Microservies/README.md
+++ ECommerce-Microservies/README.md
@@ -6,7 +6,8 @@
     * order-service:
     * product-service:
 
-### keycloack
+### Keycloack
 * Getting started docker: docker run -p 8181:8080 -e KEYCLOAK_ADMIN=admin -e KEYCLOAK_ADMIN_PASSWORD=admin quay.io/keycloak/keycloak:18.0.0 start-dev
+    
 
     
--- ECommerce-Microservies/discovery-server/src/main/java/com/ngochung/discoveryserver/config/SecurityConfig.java
+++ ECommerce-Microservies/discovery-server/src/main/java/com/ngochung/discoveryserver/config/SecurityConfig.java
@@ -3,47 +3,38 @@ package com.ngochung.discoveryserver.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-//@Configuration
+@Configuration
 public class SecurityConfig {
-//    @Value("${app.eureka.username}")
-//    private String username;
-//    @Value("${app.eureka.password}")
-//    private String password;
+    @Value("${app.eureka.username}")
+    private String username;
+    @Value("${app.eureka.password}")
+    private String password;
 
-   /* @Bean
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf()
-                .disable()
-                .authorizeRequests()
-                .antMatchers(HttpMethod.DELETE)
-                .hasRole("ADMIN")
-                .antMatchers("/admin/**")
-                .hasAnyRole("ADMIN")
-                .antMatchers("/user/**")
-                .hasAnyRole("USER", "ADMIN")
-                .antMatchers("/login/**")
-                .anonymous()
-                .anyRequest()
+        http.csrf().disable()
+                .authorizeRequests().anyRequest()
                 .authenticated()
                 .and()
-                .httpBasic()
-                .and()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                .httpBasic();
 
         return http.build();
     }
 
     @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.debug(securityDebug)
-                .ignoring()
-                .antMatchers("/css/**", "/js/**", "/img/**", "/lib/**", "/favicon.ico");
-    }*/
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username(username)
+                .password(password)
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
 }
