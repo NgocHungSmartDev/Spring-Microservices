6000df7c6f708d68fc34fb0812cb7886148bfabe
NgocHungSmartDev <hoang.ngoc.hungit@gmail.com>
2023-02-26 11:11:02 +0700

    Microservices | Fix bug place order and upgrade version spring cloud

--- ECommerce-Microservies/order-service/src/main/java/com/ngochung/orderservice/config/WebClientConfig.java
+++ ECommerce-Microservies/order-service/src/main/java/com/ngochung/orderservice/config/WebClientConfig.java
@@ -10,7 +10,7 @@ public class WebClientConfig {
 
     @Bean
     @LoadBalanced
-    public WebClient webClient() {
-        return WebClient.builder().build();
+    public WebClient.Builder webClient() {
+        return WebClient.builder();
     }
 }
