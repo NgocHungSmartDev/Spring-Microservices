773519a96b4f2ea88d6da53071033fb8c7b77a13
NgocHungSmartDev <hoang.ngoc.hungit@gmail.com>
2023-02-27 20:36:44 +0700

    Microservices | Apply Circuitbreaker

--- ECommerce-Microservies/order-service/src/main/java/com/ngochung/orderservice/service/OrderService.java
+++ ECommerce-Microservies/order-service/src/main/java/com/ngochung/orderservice/service/OrderService.java
@@ -3,5 +3,5 @@ package com.ngochung.orderservice.service;
 import com.ngochung.orderservice.dto.OrderRequest;
 
 public interface OrderService {
-    void placeOrder(OrderRequest orderRequest);
+    String placeOrder(OrderRequest orderRequest);
 }
